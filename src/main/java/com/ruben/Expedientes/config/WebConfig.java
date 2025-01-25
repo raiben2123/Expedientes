9c5275b25f6eca9292b028d06e116b16a32b3a91
raiben2123 <104256843+raiben2123@users.noreply.github.com>
2025-01-25 19:57:26 +0100

    Preparando deploy (MariaDB)

--- src/main/java/com/ruben/Expedientes/config/WebConfig.java
+++ src/main/java/com/ruben/Expedientes/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ruben.Expedientes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Cambia esto si necesitas un mapeo diferente
-                .allowedOrigins("http://localhost") // Cambia esto a la URL de tu frontend
+                .allowedOrigins("*") // Permite todos los orígenes
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                .allowedHeaders("*"); // Permite todos los encabezados
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permite credenciales (si es necesario)
     }
 }
